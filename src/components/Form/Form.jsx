59e5ecb8f8d431cc7b16eb2d8f84e1815ac8e247
XanderReynolds <xane514@gmail.com>
2021-06-18 08:42:00 -0400

    created some logic for card component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,23 +2,73 @@ import React from 'react';
 import InputBase from '../InputBase/InputBase.jsx'
 import './Form.css';
 
+const INIT_CARD = {  
+    card: '',
+    cardHolder: '',
+    expiry: '',
+    securityCode: '',  
+}
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
+      cardData: INIT_CARD,
+      maxLength: 19
+    }
+  }
 
+  handleInputData = (e) => {
+
+    if(e.target.name === 'card'){
+      let mask = e.target.value.split(' ').join('');
+      if(mask.length){
+        mask = mask.match(new RegExp('.{1,4}', 'g')).join(' ');
+        this.setState((prevState) => ({       
+          cardData: { 
+           ...prevState.cardData, 
+            [e.target.name]: mask}
+           }));
+      } else {
+        this.setState((prevState) => ({       
+          cardData: { 
+            ...prevState.cardData, 
+            [e.target.name]: ''}
+            }));
+      }
+    } else {
+      this.setState((prevState) => ({       
+        cardData: { 
+          ...prevState.cardData, 
+          [e.target.name]: e.target.value}
+          }));
     }
   }
 
   render() {
+
+    const inputData = [
+      {label: 'Card Number',         name:'card',   type:'text' },
+      {label: 'CardHolder\'s Name',  name:'cardHolder',   type:'text' },
+      {label: 'Expiry Date (MM/YY)', name:'expiry',       type:'text' },
+      {label: 'Security Code',       name:'securityCode', type:'text' },
+    ]
+    console.log(inputData.length);
+
     return (
       <div>
         <h1>Add New Card</h1>
         <form action="">
-          <InputBase />
-          <InputBase />
-          <InputBase />
-          <InputBase />
+          {inputData.length ? inputData.map((item) => {
+            return <InputBase 
+            placeholder={item.label}
+            type={item.type}
+            value={this.state.cardData && this.state.cardData[item.name]} //Check for data before rendering
+            onChange={this.handleInputData}
+            autoComplete="off"
+            maxLength={this.state.maxLength}
+            name={item.name}
+            />
+          }) : null}         
           <div>
           <InputBase type="submit" value="Add Card"/>
           </div>
--- src/components/InputBase/InputBase.jsx
+++ src/components/InputBase/InputBase.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './InputBase.css';
 
 const InputBase = (props) => (
-  <input {...props} />
+  <label>
+    <input className="input-root" {...props} />
+  </label>
 )
 
-export default InputBase
+export default InputBase;
